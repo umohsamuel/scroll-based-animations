7607ab73fab9a23c084fde213eb6525702a54c18
bigSam <umohsg@gmail.com>
2026-07-11 03:13:14 +0100

    sharp, working functionality

--- src/components/scroll-zoom/scroll-zoom.tsx
+++ src/components/scroll-zoom/scroll-zoom.tsx
@@ -14,7 +14,7 @@ export default function ScrollZoom() {
           start: "top top",
           end: "+=5000",
           scrub: true,
-          markers: true,
+          // markers: true,
           pin: ".experience",
         },
       });
@@ -23,6 +23,23 @@ export default function ScrollZoom() {
         "--progress1": 0.4,
         ease: "none",
       });
+
+      exp.to(
+        ".experience-top",
+        {
+          xPercent: -200,
+          ease: "none",
+        },
+        "<"
+      );
+      exp.to(
+        ".experience-bottom",
+        {
+          xPercent: 200,
+          ease: "none",
+        },
+        "<"
+      );
 
       exp.from(".extraBox", {
         scaleX: 0,
@@ -45,54 +62,9 @@ export default function ScrollZoom() {
           <div className="spacer-end"></div>
         </div>
       </div>
-      <section>another section</section>
+      <section className="bg-linear-to-b from-white to-black">
+        another section
+      </section>
     </div>
   );
-
-  // useGSAP(
-  //   () => {
-  //     const exp = gsap.timeline({
-  //       scrollTrigger: {
-  //         trigger: ".experience",
-  //         start: "top top",
-  //         end: "+=5000",
-  //         scrub: true,
-  //         markers: true,
-  //         pin: ".experience",
-  //       },
-  //     });
-
-  //     exp.to(".experience-middle", {
-  //       "--progress1": 1,
-  //       ease: "none",
-  //       smoothOrigin: true,
-  //     });
-  //     exp.from(
-  //       ".extraBox",
-  //       {
-  //         scaleX: 0,
-  //         ease: "none",
-  //       },
-  //       "-=0.4"
-  //     );
-  //   },
-  //   { scope: container }
-  // );
-
-  // return (
-  //   <div ref={container} className="w-full h-full m-0">
-  //     <div className="experience">
-  //       <div className="extraBox"></div>
-  //       <div className="experience-show">
-  //         <h1 className="experience-top">Let Me show you</h1>
-  //         <div className="experience-middle-container">
-  //           <h1 className="experience-middle font-panchang">Enter T Enter</h1>
-  //         </div>
-  //         <h1 className="experience-bottom">My Magic Trick</h1>
-  //         <div className="spacer-end"></div>
-  //       </div>
-  //     </div>
-  //     <section>another section</section>
-  //   </div>
-  // );
 }
