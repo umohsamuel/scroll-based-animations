f6ca851ca4b6e4c5463544108f6b20d997e26223
bigSam <umohsg@gmail.com>
2026-07-11 19:31:17 +0100

    yup, i think im happy with this

--- src/components/scroll-zoom/scroll-zoom.tsx
+++ src/components/scroll-zoom/scroll-zoom.tsx
@@ -8,6 +8,14 @@ export default function ScrollZoom() {
 
   useGSAP(
     () => {
+      gsap.to(".animation-bottom", {
+        y: -15,
+        duration: 0.8,
+        repeat: -1,
+        yoyo: true,
+        ease: "power1.inOut",
+      });
+
       const tl = gsap.timeline({
         scrollTrigger: {
           trigger: ".animation",
@@ -33,6 +41,7 @@ export default function ScrollZoom() {
         },
         0
       );
+
       tl.to(
         ".animation-bottom",
         {
@@ -64,20 +73,23 @@ export default function ScrollZoom() {
     <div ref={container} className="w-full h-full m-0 bg-dark text-light">
       <div className="animation relative w-full h-[100vh] z-10 overflow-hidden">
         <div className="extraBox absolute inset-0 w-full h-full z-10 pointer-events-none origin-center bg-light" />
-        <div className="text-5xl font-bold">
-          <h1 className="animation-top absolute top-[5%] left-[2.5%] text-5xl font-bold">
-            Hot EBA dey for you
+        <div className="text-6xl font-bold uppercase">
+          <h1 className="animation-top absolute top-[5%] tracking-wide left-[2.5%]">
+            scroll <span className="text-light/60">based</span> <br />{" "}
+            <span className="font-archiDaughter tracking-widest">
+              animations
+            </span>
           </h1>
           <div className="inset-0 absolute grid place-items-center pointer-events-none">
-            <h1 className="animation-middle origin-center font-black whitespace-nowrap text-[clamp(4rem,18vw,15rem)] text-center">
+            <h1 className="animation-middle origin-center font-black whitespace-nowrap text-[clamp(4rem,18vw,15rem)] text-center ">
               <br />
-              ENTER
+              enter
               <br />
-              UMOH
+              umoh
               <br />
             </h1>
           </div>
-          <h1 className="animation-bottom right-[2.5%] bottom-[5%] absolute">
+          <h1 className="animation-bottom right-[2.5%] bottom-[5%] absolute font-archiDaughter tracking-widest ">
             Scroll down to see
           </h1>
           <div className="spacer-end"></div>
